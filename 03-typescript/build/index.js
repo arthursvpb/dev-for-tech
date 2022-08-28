5690d94c3d3c5f1bfa50df9ddfe1b88f629e4e2e
arthursvpb <arthurandroid15@gmail.com>
2022-08-28 20:01:38 -0300

    add identity and convert generic examples

--- 03-typescript/build/index.js
+++ 03-typescript/build/index.js
@@ -5,5 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (_, res) => res.send('Hello World'));
+app.get('/identity', (req, res) => {
+    const names = ['john', 'arthur', 'thomas'];
+    const numbers = [1, 2, 3];
+    const personArray = [{ name: 'Arthur', age: 23 }];
+    function identity(arg) {
+        return arg;
+    }
+    const typedNames = identity(names);
+    const typedNumbers = identity(numbers);
+    const typedObject = identity(personArray);
+    res.send({ typedNames, typedNumbers, typedObject });
+});
+app.get('/convert', (req, res) => {
+    const { number = '' } = req.query;
+    const convert = (number) => +number;
+    const result = convert(number);
+    res.send({ result });
+});
 app.listen(8080, () => console.log('✨ Listening...'));
--- 03-typescript/src/index.ts
+++ 03-typescript/src/index.ts
@@ -1,8 +1,43 @@
-import { Request, Response } from 'express';
 import express from 'express';
 
 const app = express();
+app.get('/', (_, res) => res.send('Hello World'));
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World'));
+// Using generics https://www.typescriptlang.org/docs/handbook/2/generics.html
+interface Person {
+  name: string;
+  age: number;
+}
+
+app.get('/identity', (req, res) => {
+  const names = ['john', 'arthur', 'thomas'];
+  const numbers = [1, 2, 3];
+  const personArray: Person[] = [{ name: 'Arthur', age: 23 }];
+
+  function identity<Type>(arg: Type[]) {
+    return arg;
+  }
+
+  const typedNames = identity(names);
+  const typedNumbers = identity(numbers);
+  const typedObject = identity<Person>(personArray);
+
+  res.send({ typedNames, typedNumbers, typedObject });
+});
+
+// http://localhost:8080/convert?number=10
+interface ReqQuery {
+  number: string;
+}
+
+app.get<{}, {}, {}, unknown>('/convert', (req, res) => {
+  const { number = '' } = req.query as ReqQuery;
+
+  const convert = (number: string) => +number;
+
+  const result = convert(number);
+
+  res.send({ result });
+});
 
 app.listen(8080, () => console.log('✨ Listening...'));
