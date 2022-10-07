32cbf61dbe2130f50829a502ebeb7d0490894a06
arthursvpb <arthurandroid15@gmail.com>
2022-10-06 21:25:42 -0300

    add auth and private routes

--- 04-react/src/pages/Public/index.tsx
+++ 04-react/src/pages/Public/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-export const Contact: React.FC = () => {
+export const Public: React.FC = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <h1>Not authenticated</h1>
     </div>
   );
 };
